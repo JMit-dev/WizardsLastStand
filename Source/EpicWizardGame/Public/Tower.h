1c3d06f611a9bc72553e7636956e6893169ade51
CodingCarlos23 <codingcarlos23@gmail.com>
2025-11-10 23:24:21 -0500

    feat: added courtyard level layout and tower object

---
+++ Source/EpicWizardGame/Public/Tower.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tower.generated.h"
+
+UCLASS()
+class EPICWIZARDGAME_API ATower : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATower();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// The mesh that will show in the world
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* TowerMesh;
+};
